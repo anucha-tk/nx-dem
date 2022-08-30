35be85088e3abd99371f7eb5ec4d8aedfa14e805
Anucha.tk <anuchataowkaen@gmail.com>
2022-08-30 22:09:04 +0700

    client

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,8 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
+import { useEffect } from 'react';
 
 export function App() {
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  }, []);
+
   return (
     <>
       <NxWelcome title="client" />
